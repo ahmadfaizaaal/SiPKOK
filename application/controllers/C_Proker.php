fccc88c805d11da76b5b30937182396b1b5196ff
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2017-12-05 01:59:06 +0700

    tambahProker with dokumen fixed

--- application/controllers/C_Proker.php
+++ application/controllers/C_Proker.php
@@ -5,13 +5,14 @@
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('M_Proker');
+		$this->load->model('M_Dokumen');
 		// $this->load->controller("C_Organisasi");
 	}
 
 	public function hapusProker($idProker){
 		$this->db->where('idProker', $idProker);
 		$this->db->delete('proker');
-		$this->session->set_userdata(array('proker'=>$this->M_Proker->selectProkerByOrganisasi($this->session->userdata('akunAktif')->organisasi)),true);
+		$this->session->set_userdata(array('proker'=>$this->M_Proker->selectProkerJoinDokumen($this->session->userdata('akunAktif')->organisasi)),true);
 		redirect(base_url());
 		// $this->load->view('view_beranda_Pimpinan_Organisasi', $data);
 		// $this->
@@ -29,9 +30,84 @@
 		} else {
 			$data["jenis"] = 1;
 		}
-		// $data["jenis"] = "";
-		// $data["proposal"] = "";
-		// $data["lpj"] = "";
+
+		$allowed =  array('doc','docx' ,'pdf');
+		// Upload Proposal
+	  $namaProposal = $_FILES['fileProposal']['name'];
+	  $sizeProposal= $_FILES["fileProposal"]["size"]/1024;
+	  $tipeProposal = pathinfo($namaProposal, PATHINFO_EXTENSION);
+	  $tmpNameProposal=$_FILES["fileProposal"]["tmp_name"];  
+	  if(!in_array($tipeProposal,$allowed) ) {
+		  echo "You can only upload a Word doc/docx or a Pdf file.";
+	  } else {
+			if($sizeProposal<=200){
+
+			  //New file name
+			  $random=rand(1111,9999);
+			  $newFileName=$random."_Proposal_".$idOrganisasi."_".$namaProposal;
+
+			  //File upload path
+			  $uploadPath="assets/doc/".$newFileName;
+
+			  //function for upload file
+			  if(move_uploaded_file($tmpNameProposal,$uploadPath)){
+					echo "Successful"; 
+					echo "File Name :".$newFileName; 
+					echo "File Size :".$sizeProposal." kb"; 
+					echo "File Type :".$tipeProposal;
+					$dataDokumen["idDokumen"] = "D001";
+					$dataDokumen["namaDokumen"] = $newFileName;	
+					$dataDokumen["isiDokumen"] = $newFileName;						
+					$dataDokumen["waktuUpload"] = date("Y-m-d h:i:sa");
+					$dataDokumen["jenis"] = 0;
+					$dataDokumen["status"] = 1;
+					$this->M_Dokumen->insertDokumen($dataDokumen);
+			  }
+			}
+			else{
+			  echo "Maximum upload file size limit is 200 kb";
+			}
+	  }
+
+		// Upload LPJ
+	  $namaLpj = $_FILES['fileLpj']['name'];
+	  $sizeLpj= $_FILES["fileLpj"]["size"]/1024;
+	  $tipeLpj = pathinfo($namaLpj, PATHINFO_EXTENSION);
+	  $tmpNameLpj=$_FILES["fileLpj"]["tmp_name"];  
+	  if(!in_array($tipeLpj,$allowed) ) {
+		  echo "You can only upload a Word doc/docx or a Pdf file.";
+	  } else {
+			if($sizeLpj<=200){
+
+			  //New file name
+			  $random=rand(1111,9999);
+			  $newFileName=$random."_LPJ_".$idOrganisasi."_".$namaLpj;
+
+			  //File upload path
+			  $uploadPath="assets/doc/".$newFileName;
+
+			  //function for upload file
+			  if(move_uploaded_file($tmpNameLpj,$uploadPath)){
+					echo "Successful"; 
+					echo "File Name :".$newFileName; 
+					echo "File Size :".$sizeLpj." kb"; 
+					echo "File Type :".$tipeLpj; 
+					$dataDokumen["idDokumen"] = "D001";
+					$dataDokumen["namaDokumen"] = $newFileName;	
+					$dataDokumen["isiDokumen"] = $newFileName;						
+					$dataDokumen["waktuUpload"] = date("Y-m-d h:i:sa");
+					$dataDokumen["jenis"] = 1;
+					$dataDokumen["status"] = 1;
+					$this->M_Dokumen->insertDokumen($dataDokumen);
+			  }
+			}
+			else{
+			  echo "Maximum upload file size limit is 200 kb";
+			}
+	  }	   
+		
+		$data["proposal"] = $this->M_Dokumen->getLatestDokumen(0)->idDokumen;
+		$data["lpj"] = $this->M_Dokumen->getLatestDokumen(1)->idDokumen;		
 		$this->M_Proker->insertProker($data);
 		$this->session->set_userdata(array('proker'=>$this->M_Proker->selectProkerJoinDokumen($this->session->userdata('akunAktif')->organisasi)),true);
 		// echo "<script type='text/javascript'>alert('Tambah Program Kerja Sukses');</script>";
--- application/models/M_Dokumen.php
+++ application/models/M_Dokumen.php
@@ -2,12 +2,20 @@
 	// public function index(){
 	// 	echo "M_Dokumen";
 	// }
-	public function getStatus($idDokumen){
-		$query = $this->db->where("idDokumen",$idDokumen);
-		$query = $this->db->get('dokumen');
-		$hasil = $query->result();
-		return $hasil[0]->status;
+	// public function getStatus($idDokumen){
+	// 	$query = $this->db->where("idDokumen",$idDokumen);
+	// 	$query = $this->db->get('dokumen');
+	// 	$hasil = $query->result();
+	// 	return $hasil[0]->status;
+	// }
+	public function insertDokumen($data){
+		$this->db->insert("dokumen", $data);
 	}
-	
+	public function getLatestDokumen($jenis){
+		$query = $this->db->where('jenis',$jenis);        
+        $query = $this->db->order_by("idDokumen", "desc");
+        $query = $this->db->get('dokumen');
+        return $query->result()[0];
+	}
 }
 ?>
